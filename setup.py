3e4dc45aa7c25e972c7ee1bc6b7e54fba334c0bc
Yask Srivastava <yask123@gmail.com>
2016-05-01 15:19:36 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+
+from setuptools import setup
+
+setup(name="ims",
+	version="1.0",
+	description="Instantly stream movies/ tv episodes",
+	url="iyask.me",
+	author="Yask Srivastava",
+	author_email="yask123@gmail.com",
+	license='MIT',
+	packages=["ims"],
+	scripts=["bin/ims"],
+	zip_safe=False)
