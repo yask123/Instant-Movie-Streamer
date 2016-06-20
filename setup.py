bb195dd692dccb266fc66caa95ee9858c5046369
Yask Srivastava <yask123@gmail.com>
2016-06-20 16:24:02 +0530

    updated

---
+++ setup.py
@@ -0,0 +1,16 @@
+
+from setuptools import setup
+
+setup(name="ims",
+	  version="2.6",
+	  description="Instantly stream movies/ tv episodes",
+	  url="http://iyask.me",
+	  author="Yask Srivastava",
+	  author_email="yask123@gmail.com",
+	  license='MIT',
+	  packages=["ims"],
+	  scripts=["bin/ims"],
+	  install_requires=[
+		  'BeautifulSoup4',
+		  'requests'],
+	  zip_safe=False)
